0657f866d30df2e93de6326d362283cb6d6c9d0f
Jin Hong Kim <happyhappyjinjin@outlook.com>
2024-02-14 22:19:13 -0500

    Added exercises

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -168,3 +168,10 @@ int main()
 	glfwTerminate();
 	return 0;
 }
+/* 5.8 Exercises
+ To really get a good grasp of the concepts discussed a few exercises were set up.
+ It is advised to work through them before continuing to the next subject to make sure
+ you get a good grasp of what’s going on. */
+	//! 1. Try to draw 2 triangles next to each other using glDrawArrays by adding more vertices to your data.Solution: / src / 1.getting_started / 2.3.hello_triangle_exercise1 /.
+	//! 2. Now create the same 2 triangles using two different VAOs and VBOs for their data. Solution : / src / 1.getting_started / 2.4.hello_triangle_exercise2 /.
+	//! 3. Create two shader programs where the second program uses a different fragment shader that outputs the color yellow; draw both triangles again where one outputs the color yellow. Solution: / src / 1.getting_started / 2.5.hello_triangle_exercise3 /.
