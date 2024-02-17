aff6eaca9de56e9a926c7a2e74892bb530fa74a9
Jin Hong Kim <happyhappyjinjin@outlook.com>
2024-02-17 15:04:49 -0500

    Modified the vertexShaderSource and fragmentShaderSource.

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -14,18 +14,18 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
-"}\n\0";
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+	"}\n\0";
 
 int main()
 {
