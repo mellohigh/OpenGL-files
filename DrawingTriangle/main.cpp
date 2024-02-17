412d793dd5b6c4b890f9d51b80b619b858fbe5f5
Jin Hong Kim <happyhappyjinjin@outlook.com>
2024-02-17 15:20:56 -0500

    C++11 raw string literals for vertexShaderSource and fragmentShaderSource.

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -22,11 +22,12 @@ const char* vertexShaderSource = R"END(
 	}
 	)END";
 
-const char* fragmentShaderSource = R"END(#version 330 core
+const char* fragmentShaderSource = R"END(
+	#version 330 core
 	out vec4 FragColor;
 	void main()
 	{
-	   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+		FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 	}
 	)END";
 
--- TriangleShader/main.cpp
+++ TriangleShader/main.cpp
@@ -13,20 +13,24 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char* vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main()\n"
-	"{\n"
-	"	gl_Position = vec4(aPos, 1.0);\n"
-	"}\0";
+const char* vertexShaderSource = R"END(
+	#version 330 core
+	layout (location = 0) in vec3 aPos;
+	void main()
+	{
+		gl_Position = vec4(aPos, 1.0);
+	}
+	)END";
 
-const char* fragmentShaderSource = "#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"uniform vec4 ourColor;\n"
-	"void main()\n"
-	"{\n"
-	"   FragColor = ourColor;\n"
-	"}\n\0";
+const char* fragmentShaderSource = R"END(
+	#version 330 core
+	out vec4 FragColor;
+	uniform vec4 ourColor;
+	void main()
+	{
+	   FragColor = ourColor;
+	}
+	)END";
 
 int main()
 {
